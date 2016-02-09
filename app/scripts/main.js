f26e3a6dac0d493ebabb6c11e41e424e421166b7
Manikandan Dhayalan <dhayalam@advisory.com>
2016-02-09 16:04:53 +0530

    Initial Commit

---
+++ app/scripts/main.js
@@ -0,0 +1,18 @@
+/*global shar-2-do, $*/
+
+
+window.shar-2-do = {
+  Models: {},
+  Collections: {},
+  Views: {},
+  Routers: {},
+  init: function () {
+    'use strict';
+    console.log('Hello from Backbone!');
+  }
+};
+
+$(document).ready(function () {
+  'use strict';
+  shar-2-do.init();
+});
