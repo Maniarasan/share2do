f83e9119376859c0f53009ef7d9aabcdec8b6393
Manikandan Dhayalan <dhayalam@advisory.com>
2016-02-10 11:25:17 +0530

    Added initial bootstrap based index page

--- app.js
+++ app.js
@@ -21,9 +21,11 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(__dirname + '/views'));
 
 app.use('/', routes);
 app.use('/users', users);
+app.use('/scripts', express.static(__dirname + '/node_modules/bootstrap/dist/'));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
