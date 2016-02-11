e1d993b33153267bd9d37855ec4756a1ba4e2edf
Arasu <arasusern16@gmail.com>
2016-02-11 16:30:10 +0530

    update-task completed

--- routes/update-task.js
+++ routes/update-task.js
@@ -1,34 +1,16 @@
 'use strict';
 
 var Task = require('../models/task');
-var data = {
-  objectId: '56bb251e9243f208117a5ecb'
+module.exports = function (req, res) {
+  Task.findOne({_id: req.params.taskid}, '', function (err, task) {
+    if (err) { console.log(err); }
+    task.title = req.body.title;
+    task.completed = req.body.completed;
+    task.save(function (err, task) {
+		if (err) { console.log(err); }
+		res.json(task);
+	});
+  });
 };
 
-
-
-// var updatetask = function (req, res) {
-//   Task.find(data, function (err, result) {
-//     if (err) { console.log(result); }
-
-//     result.objectId = '6bb251e9243f208117a';
-//     result.save(function (err) {
-//         if (!err) {
-//             res.json("updated");
-//         }
-//     });
-//     res.json('your data updated');
-//   });
-// };
-
-/*var updatetask = function (req, res) {
-    Task.update({_id: '56bc11b0d30e870c04057aa2'}, function (err) {
-        if (!err) {
-            res.json("updated");
-        }
-    });
-};
-// */
-// var updatetask;
-// module.exports = updatetask;
 
